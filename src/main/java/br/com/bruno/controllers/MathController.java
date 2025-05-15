e008ccc6d679cfc02c9cdc356380c37052a04997
Bruno Freitas <brunofreitasd11@gmail.com>
2025-05-15 20:59:37 -0300

    implementando função de soma

--- src/main/java/br/com/bruno/controllers/MathController.java
+++ src/main/java/br/com/bruno/controllers/MathController.java
@@ -1,10 +1,22 @@
 package br.com.bruno.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.bruno.SimpleMath;
 
 @RestController
 public class MathController {
+	
+	SimpleMath math = new SimpleMath();
+	
+	@GetMapping("/sum1/{num1}/{num2}")
+	public Double sum(
+			@PathVariable Double num1,
+			@PathVariable Double num2
+			) 
+	{return math.sum(num1, num2);}
 	
 	@GetMapping
 	public String teste() {
@@ -12,4 +24,4 @@ public class MathController {
 	}
 }
 
-}
+
